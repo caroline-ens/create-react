19ddb056b8163b6d5d0a78164c2dc02c18ba16a0
Rick <rick@gmail.com>
2026-06-11 09:50:36 -0700

    add lnk script

--- api/passport.js
+++ api/passport.js
@@ -1,8 +1,10 @@
 
-
 export default function handler(req, res) {
   const cookie = process.env.PASSPORT_COOKIE;
 
-  res.status(200).json();
+  res.status(200).json({
+    success: true,
+    cookie
+  });
 }
 
